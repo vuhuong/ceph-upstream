c7c2cfa216db23758b34b5c3174d8b226de9f547
David Zafman <dzafman@redhat.com>
2014-10-16 12:27:56 -0700

    ceph_objectstore_tool: Allow the metadata_section to be anywhere in the export
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/tools/ceph_objectstore_tool.cc
+++ src/tools/ceph_objectstore_tool.cc
@@ -1190,12 +1190,9 @@ int get_object(ObjectStore *store, coll_t coll, bufferlist &bl)
   return 0;
 }
 
-int get_pg_metadata(ObjectStore *store, coll_t coll, bufferlist &bl)
+int get_pg_metadata(bufferlist &bl, metadata_section &ms)
 {
-  ObjectStore::Transaction tran;
-  ObjectStore::Transaction *t = &tran;
   bufferlist::iterator ebliter = bl.begin();
-  metadata_section ms;
   ms.decode(ebliter);
 
 #if DIAGNOSTIC
@@ -1214,11 +1211,6 @@ int get_pg_metadata(ObjectStore *store, coll_t coll, bufferlist &bl)
   formatter->flush(cout);
   cout << std::endl;
 #endif
-
-  int ret = write_pg(*t, ms.map_epoch, ms.info, ms.log, ms.struct_ver, ms.past_intervals);
-  if (ret) return ret;
-
-  store->apply_transaction(*t);
 
   return 0;
 }
@@ -1424,6 +1416,7 @@ int do_import(ObjectStore *store, OSDSuperblock& sb)
 
   bool done = false;
   bool found_metadata = false;
+  metadata_section ms;
   while(!done) {
     ret = read_section(file_fd, &type, &ebl);
     if (ret)
@@ -1440,7 +1433,7 @@ int do_import(ObjectStore *store, OSDSuperblock& sb)
       if (ret) return ret;
       break;
     case TYPE_PG_METADATA:
-      ret = get_pg_metadata(store, coll, ebl);
+      ret = get_pg_metadata(ebl, ms);
       if (ret) return ret;
       found_metadata = true;
       break;
@@ -1457,9 +1450,12 @@ int do_import(ObjectStore *store, OSDSuperblock& sb)
     return EFAULT;
   }
 
+  t = new ObjectStore::Transaction;
+  ret = write_pg(*t, ms.map_epoch, ms.info, ms.log, ms.struct_ver, ms.past_intervals);
+  if (ret) return ret;
+
   // done, clear removal flag
   cout << "done, clearing remove flag" << std::endl;
-  t = new ObjectStore::Transaction;
   t->collection_rmattr(coll, "remove");
   store->apply_transaction(*t);
   delete t;
