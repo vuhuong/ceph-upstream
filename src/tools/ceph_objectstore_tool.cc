e3137985d83782ae83fb1525419acd79b72ff862
Sage Weil <sage@redhat.com>
2014-11-23 12:30:52 -0800

    ceph_objectstore_tool: skip pgmeta objects in 'list' command
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/tools/ceph_objectstore_tool.cc
+++ src/tools/ceph_objectstore_tool.cc
@@ -1482,7 +1482,8 @@ int do_list(ObjectStore *store, coll_t coll, Formatter *formatter)
       return r;
     for (vector<ghobject_t>::iterator i = objects.begin();
 	 i != objects.end(); ++i) {
-
+      if (i->is_pgmeta())
+	continue;
       formatter->open_object_section("list");
       i->dump(formatter);
       formatter->close_section();
